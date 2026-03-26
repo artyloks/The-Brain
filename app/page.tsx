a8715432c211e1a92e2e0951380250ce7b91c5fe
artyloks <229499917+artyloks@users.noreply.github.com>
2026-03-25 20:11:45 -0700

    system: internal recovery

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return (<main style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}><h1>THE BRAIN IS ONLINE</h1></main>)
+}
